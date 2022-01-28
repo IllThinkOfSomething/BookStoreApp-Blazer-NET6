a111337de73daf06d6af41577614f4a2bcfc3fe8
IllThinkOfSomething <dziugis10@gmail.com>
2022-01-29 00:16:15 +0100

    Added EF Core and Scuffoolded the Database Add project files.

---
+++ BookStoreAPI/Program.cs
@@ -0,0 +1,48 @@
+using BookStoreAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+//Entity framework allows to connect to multiple db engines, not only SQL Server (For example Postgres)
+builder.Services.AddDbContext<NET6MSIContext>(options => options.UseSqlServer(connString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//ctx = Context, lc = Logging Config
+builder.Host.UseSerilog((ctx, lc) => 
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+
+//Restrictions on who can join API
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowAnyOrigin());    
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//Enable Cross-Origin Rescources Sharing
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
