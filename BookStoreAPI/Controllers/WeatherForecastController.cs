d34afee61b195de75dbb14aa9577be53f9a6a488
IllThinkOfSomething <dziugis10@gmail.com>
2022-01-29 00:16:15 +0100

    Add project files.

---
+++ BookStoreAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Made call to weather endpoint");
+            try
+            {
+                throw new Exception("This is out logging test exception");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Fatal Error occured");
+                throw;
+            }
+            
+        }
+    }
+}
